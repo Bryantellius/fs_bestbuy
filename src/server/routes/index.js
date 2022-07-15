1ec391339fb29cb5fa88736fa39d4843539575d7
Ben <brbryant2639@gmail.com>
2022-07-15 13:45:50 -0500

    UPDATE: walkthrough p1

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import productRouter from "./product.router";
 
 const router = express.Router();
 
 router.get("/test", (req, res, next) => {
   res.send("Hello World!");
 });
+
+router.use("/products", productRouter);
 
 export default router;
